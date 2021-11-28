d8334bc23a04dd9a3d56cc3dafdff0f2892ab568
Evan Whitehouse <ew2729@columbia.edu>
2021-11-28 09:17:17 -0500

    View code complete

--- view/view.c
+++ view/view.c
@@ -1,33 +1,65 @@
+/* Contains all functions for displaying information to the user based on possible menu states.
+ *
+ * Author: Evan Whitehouse
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include "../model/ticket.h"
 
+// Print labels for each ticket information category, with fixed column widths.
 void printLabels(){
 	printf("%-5s|%-28s|%-21s|%-21s|%-9s|%-8s\n", "ID", "Subject",
 			"Created At", "Updated At", "Priority", "Status");
 }
 
+// Display the menu for page 0 (can't scroll backwards).
 void displayMenuStart(){
-
+	printf("Enter 2 to advance, or 3 to search for a ticket by ID. \n");
 }
 
+// Display the menu for the last page (can't scroll forwards).
 void displayMenuEnd(){
-
+	printf("Enter 1 to scroll backward, or 3 to search for a ticket by ID. \n");
 }
 
+// Display the menu for when a single ticket is being viewed.
 void displayMenuSingleStruct(){
-
+	printf("Enter 1 to return to list view, or 3 to search for a ticket by ID. \n");
 }
 
-void displayTicketArray(int page, struct ticket **tickets){
+/* Display a range of 25 tickets, based on a page number that indicates which group of 25 to show.
+ *
+ * Parameters: 
+ * int page, the page number to display. Prints tickets from page * 25 to page * 25 + 25.
+ * int ticketCount, the number of tickets in the array. Ensure we won't print past that number of tickets.
+ * struct ticket **tickets, the list of tickets to print from.
+ */
+void displayTicketArray(int page, int ticketCount, struct ticket **tickets){
+	int max = 0;
+	if (page * 25 + 25 > ticketCount){
+		max = ticketCount;
+	}
+	else {
+		max = page * 25 + 25;
+	}
+	for(int i = page * 25; i < max; i++){
+		printf("%-5.5s|%-28.28s|%-21.21s|%-21.21s|%-9.9s|%-8.8s\n", tickets[i] -> id, tickets[i] -> subject,
+				tickets[i] -> created_at, tickets[i] -> updated_at, tickets[i] -> priority, tickets[i] -> status);
 
+	}
 }
 
+/* Print a single ticket with a detailed description.
+ *
+ * Parameters:
+ * int id, the ID of the ticket being printed
+ * struct ticket **tickets, the list of tickets to print from.
+ */
 void displaySingleTicket(int id, struct ticket **tickets){
-
-}
-
-int main(){
 	printLabels();
-	return 0;
+	printf("%-5.5s|%-28.28s|%-21.21s|%-21.21s|%-9.9s|%-8.8s\n", tickets[id - 1] -> id, tickets[id - 1] -> subject,
+	tickets[id - 1] -> created_at, tickets[id - 1] -> updated_at, tickets[id - 1] -> priority, tickets[id - 1] -> status);
+	printf("%s\n", tickets[id - 1] -> description);
 }
+
